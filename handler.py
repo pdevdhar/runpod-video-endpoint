d5ba6601ac7730dd08e1695f41823e641bfd051b
Prashant Devdhar <prashantdevdhar@Prashants-iMac-2020.local>
2026-05-11 20:05:37 -0700

    real image decoding

--- handler.py
+++ handler.py
@@ -1,11 +1,28 @@
 import runpod
+import base64
+from PIL import Image
+import io
+
+def decode_image(image_base64):
+    if not image_base64:
+        return None
+
+    if "," in image_base64:
+        image_base64 = image_base64.split(",")[1]
+
+    img_bytes = base64.b64decode(image_base64)
+    return Image.open(io.BytesIO(img_bytes))
+
 
 def handler(job):
-    image = job["input"].get("image_base64")
+    image_b64 = job["input"].get("image_base64")
+
+    image = decode_image(image_b64)
 
     return {
         "received": image is not None,
-        "size": len(image) if image else 0
+        "image_size": image.size if image else None
     }
+
 
 runpod.serverless.start({"handler": handler})
